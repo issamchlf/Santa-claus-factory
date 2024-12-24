3f07b644d27d6212d23b1d273709a836e1a1d16c
issamchlf <issamchellaf734@gmail.com>
2024-12-24 16:23:51 +0100

    feat:add routes of table kids

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,9 @@ Route::delete('/elve/{id}',[ToyController::class,'destroy'])->name('apidestroy')
 Route::post('/elve',[ToyController::class,'store'])->name('apistore');
 Route::put('/elve/{id}',[ToyController::class,'update'])->name('apiupdate');
 Route::get('/elve/{id}', [ToyController::class, 'show'])->name('apishow');
+
+Route::get('/santa', [KidController::class, 'index'])->name('apiindex');
+Route::delete('/santa/{id}',[KidController::class,'destroy'])->name('apidestroy');
+Route::post('/santa',[KidController::class,'store'])->name('apistore');
+Route::put('/santa/{id}',[KidController::class,'update'])->name('apiupdate');
+Route::get('/santa/{id}', [KidController::class, 'show'])->name('apishow');
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KidController;
 use App\Http\Controllers\ToyController;
 
 Route::get('/', function () {return view('home');});
 
-Route::get('/elve', [ToyController::class, 'index'])->name('home');
+Route::get('/elve', [ToyController::class, 'index'])->name('elve');
+Route::get('/santa', [KidController::class, 'index'])->name('santa');
