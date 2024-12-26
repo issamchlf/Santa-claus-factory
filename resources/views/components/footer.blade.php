9ce1a25331e9c58e6652acd35737b2cce5532681
mrene42 <mariarenebarco.969@gmail.com>
2024-12-26 15:14:46 +0100

    style: header and footer add github account

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,37 +1,80 @@
 <footer
-    class="bg-[url(https://img.freepik.com/vector-premium/mercado-navidad-al-aire-libre-puestos-abeto-casas-madera-decoradas-guirnaldas-ramas-abeto-lazos-quioscos-madera-navidad-dulces-bebidas-calientes-ilustracion-vectorial-dibujos-animados_87771-16018.jpg)]">
+    class="bg-cover bg-no-repeat bg-center ..." style="background-image: url(https://media.istockphoto.com/id/613792084/es/foto/elegante-%C3%A1rbol-de-navidad-con-estrellas.jpg?s=1024x1024&w=is&k=20&c=zyWE5NCfidguc0fj29b2gruwpaweKGKEgT1JR9c3aV0=)">
     <div class="p-1 ">
-        <div class="grid grid-cols-5 text-center">
+        <div class="grid grid-cols-11 text-center">
+            <div class="transform hover:scale-110 transition-transform duration-500">
+                <a href="">
+                    <p class="font-bold text-base">Issam</p>
+                    <img src="/image/github.svg" alt="" class="w-10 h-10  mx-auto mt-1 rounded-full">
+                </a>
+            </div>
+            <div class="transform hover:scale-110 transition-transform duration-500">
+                <a href="https://github.com/Mangel111111111">
+                    <p class="font-bold text-base">Fran</p>
+                    <img src="/image/github.svg" alt="" class="w-10 h-10  mx-auto mt-1 rounded-full">
+                </a>
+            </div>
+            <div class="transform hover:scale-110 transition-transform duration-500">
+                <a href="https://github.com/Kalixto73a">
+                    <p class="font-bold text-base">Matias</p>
+                    <img src="/image/github.svg" alt="" class="w-10 h-10  mx-auto mt-1 rounded-full">
+                </a>
+            </div>
+            <div class="transform hover:scale-110 transition-transform duration-500">
+                <a href="https://github.com/AntonioGuillen123">
+                    <p class="font-bold text-base">Jonathan</p>
+                    <img src="/image/github.svg" alt="" class="w-10 h-10  mx-auto mt-1 rounded-full">
+                </a>
+            </div>
+            <div class="transform hover:scale-110 transition-transform duration-500">
+                <a href="https://github.com/Manusitox360">
+                    <p class="font-bold text-base">Jeni</p>
+                    <img src="/image/github.svg" alt="" class="w-10 h-10 mx-auto mt-1 rounded-full">
+                </a>
+            </div>
+            <div class="transform hover:scale-110 transition-transform duration-500">
+                <a href="https://github.com/Manusitox360">
+                    <p class="font-bold text-base">Rene</p>
+                    <img src="/image/github.svg" alt="" class="w-10 h-10 mx-auto mt-1 rounded-full">
+                </a>
+            </div>
             <div class="transform hover:scale-110 transition-transform duration-500">
                 <a href="">
                     <p class="font-bold text-base">Santino</p>
-                    <img src="/image/github.svg" alt="" class="w-14 h-14  mx-auto mt-1 rounded-full">
+                    <img src="/image/github.svg" alt="" class="w-10 h-10  mx-auto mt-1 rounded-full">
                 </a>
             </div>
             <div class="transform hover:scale-110 transition-transform duration-500">
                 <a href="https://github.com/Mangel111111111">
                     <p class="font-bold text-base">Miguel</p>
-                    <img src="/image/github.svg" alt="" class="w-14 h-14  mx-auto mt-1 rounded-full">
+                    <img src="/image/github.svg" alt="" class="w-10 h-10  mx-auto mt-1 rounded-full">
                 </a>
             </div>
             <div class="transform hover:scale-110 transition-transform duration-500">
                 <a href="https://github.com/Kalixto73a">
                     <p class="font-bold text-base">Álvaro</p>
-                    <img src="/image/github.svg" alt="" class="w-14 h-14  mx-auto mt-1 rounded-full">
+                    <img src="/image/github.svg" alt="" class="w-10 h-10  mx-auto mt-1 rounded-full">
                 </a>
             </div>
             <div class="transform hover:scale-110 transition-transform duration-500">
                 <a href="https://github.com/AntonioGuillen123">
                     <p class="font-bold text-base">Antonio</p>
-                    <img src="/image/github.svg" alt="" class="w-14 h-14  mx-auto mt-1 rounded-full">
+                    <img src="/image/github.svg" alt="" class="w-10 h-10  mx-auto mt-1 rounded-full">
                 </a>
             </div>
             <div class="transform hover:scale-110 transition-transform duration-500">
                 <a href="https://github.com/Manusitox360">
                     <p class="font-bold text-base">Manuel</p>
-                    <img src="/image/github.svg" alt="" class="w-14 h-14 mx-auto mt-1 rounded-full">
+                    <img src="/image/github.svg" alt="" class="w-10 h-10 mx-auto mt-1 rounded-full">
                 </a>
             </div>
+        </div>
+        <div>
+            <hr class="my-3">
+        </div>
+        <div class="grid grid-cols-1 text-center">
+            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://github.com/issamchlf/Santa-claus-factory/tree/main" class="hover:underline">Santa-claus-factory™</a>. All Rights Reserved.
+            </span>
         </div>
     </div>
 </footer>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,9 +1,9 @@
-<header class="bg-[url(https://img.freepik.com/vector-premium/mercado-navidad-al-aire-libre-puestos-abeto-casas-madera-decoradas-guirnaldas-ramas-abeto-lazos-quioscos-madera-navidad-dulces-bebidas-calientes-ilustracion-vectorial-dibujos-animados_87771-16018.jpg)]">
+<header class="bg-cover bg-top bg-no-repeat bg-center" style="background-image: url(https://media.istockphoto.com/id/613792084/es/foto/elegante-%C3%A1rbol-de-navidad-con-estrellas.jpg?s=1024x1024&w=is&k=20&c=zyWE5NCfidguc0fj29b2gruwpaweKGKEgT1JR9c3aV0=)">
     <div >
-    <div class="text-center text-5xl p-2 pb-4 font-christmas font-bold">
+    <div class="text-center text-5xl p-2 pb-4 font-christmas font-bold text-sky-50">
         Christmas Toy Factory
     </div>
-    <div class="flex justify-center gap-16">
+    <div class="flex justify-center gap-8">
 
         <button type="button" class="flex text-[1.3rem] gap-2 focus:outline-none text-white bg-green-600 duration-1000 hover:bg-green-700 hover:scale-110 transition-transform font-medium rounded-lg text-sm px-[.8rem] py-2.5 me-2 mb-2">
             <p>🧝🏻</p>
@@ -12,6 +12,10 @@
         <button type="button" class="flex text-[1.3rem] gap-2 focus:outline-none text-white bg-red-600 duration-1000 hover:bg-red-700 hover:scale-110 transition-transform font-medium rounded-lg text-sm px-3 py-2.5 me-2 mb-2">
             <p>🎅🏻</p>
             <p class="font-bold">Santa Claus</p>
+        </button>
+        <button type="button" class="flex text-[1.3rem] gap-2 focus:outline-none text-white bg-amber-600 duration-1000 hover:bg-amber-700 hover:scale-110 transition-transform font-medium rounded-lg text-sm px-3 py-2.5 me-2 mb-2">
+            <p>🎁</p>
+            <p class="font-bold">Gifts</p>
         </button>
     </div>
     </div>
