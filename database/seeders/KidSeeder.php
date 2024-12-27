8a6af3ad40fd3095af104d7c17b74227e31d0c78
issamchlf <issamchellaf734@gmail.com>
2024-12-27 14:24:25 +0100

    feat:add data seeders for test

--- database/seeders/KidSeeder.php
+++ database/seeders/KidSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Kid;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class KidSeeder extends Seeder
 {
@@ -12,6 +13,20 @@ class KidSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $kids = [
+            // Toys for ages 0 and up
+            [
+                'name' => "jonathan",
+                'surname' => "jonny",
+                'foto' => "https://res.cloudinary.com/dq2kswexq/image/upload/v1734336018/ChristmasToyFactory/Rattle.jpg",
+                'age' => 12 ,
+                'gender' => "boy",
+                'atitude' => "good",
+                'country' => "spain",
+
+            ]];
+            foreach ($kids as $kid) {
+                Kid::create($kid);
+            }
     }
 }
