fcbd4054c14e56846984f70e0afe489658532e88
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2025-01-13 11:29:25 +0100

    test:kid

---
+++ tests/Feature/KidTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Kid;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class KidTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_CheckIfElfCanGetAllToysInView(){
+        $this->withoutExceptionHandling();
+
+        Kid::factory(5)->create();
+        Kid::all();
+
+        $response = $this->get('/santa');
+        $response->assertStatus(200)
+            ->assertViewIs('santa');
+    }
+
+    // public function test_CheckIfElfCanGetOneToyInView() {
+    //     $this->withoutExceptionHandling();
+
+    //     $toy = Toy::factory()->create();
+
+    //     $response = $this->get('/elf/' . $toy->id);
+
+    //     $response->assertStatus(200)
+    //         ->assertViewIs('elfShow')
+    //         ->assertViewHas('elf', $toy);
+    // }
+}
